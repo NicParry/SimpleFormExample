11d2836e297661a1e67684d28b44692d48c059db
Nic Parry <nicholasp@synaq.com>
2016-02-16 19:58:54 +0200

    Basic web test for index.php

--- src/Tests/BasicTest.php
+++ src/Tests/BasicTest.php
@@ -3,10 +3,15 @@
 namespace SimpleForm\Tests;
 
 
+use Goutte\Client;
+
 class BasicTest extends \PHPUnit_Framework_TestCase
 {
+
     public function testFirst()
     {
-        $this->assertTrue(true);
+        $client = new Client();
+        $client->request('GET', 'http://localhost');
+        $this->assertEquals('Dummy index.php output', $client->getResponse()->getContent());
     }
 }
