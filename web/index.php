5d3c3c5ef76e13331b6e3cb850fac34aa783c766
Nic Parry <nicholasp@synaq.com>
2016-02-16 19:29:47 +0200

    Composer used for Autoloading namespaced classes

--- web/index.php
+++ web/index.php
@@ -1,3 +1,8 @@
 <?php
 
-echo "Dummy index output";
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__)));
+
+require_once(ROOT . DS . 'vendor' . DS . 'autoload.php');
+
+echo "Dummy index.php output";
